84302c18c0d4ceaf3398ef762214331116eb1335
lvelazquez <leo@leonardovelazquez.com>
2016-02-22 09:30:37 -0800

    fixed issue with intro color on mobile

--- dev/js/main.js
+++ dev/js/main.js
@@ -15,100 +15,25 @@
 
 var myProjects = new ProjectList(projects);
 var isMobile = window.matchMedia("only screen and (max-width: 760px)");
-var MainRouter = Backbone.Router.extend({
-    routes: {
-        "work/:id": "showProject",
-        "work": "jumpToPage",
-        "about": "jumpToPage",
-        "contact": "jumpToPage"
-    },
-    hideProject: function () {
-        if (isMobile.matches) {
-            this.project.$el.addClass('invisible');
-        }
-    },
-    initialize: function () {
-        var _self = this;
 
-        this.thumbs = new ThumbView({
-            collection: myProjects
-        });
-
-        this.project = new ProjectView({
-            collection: myProjects
-        });
-
-        $(window).resize(function () {
-            _self.project.resize();
-        });
-
-        $('body').on('activate.bs.scrollspy', function (e) {
-            var currentPage = $('a', e.target).attr('href');
-            _self._scrolling = true;
-
-            if (currentPage != "#/") {
-                if (!_.isUndefined(_self.project) && !_.isUndefined(_self.project.currentProjectId) && currentPage == "#/work") {
-                    currentPage = currentPage + '/' + _self.project.currentProjectId;
-                }
-                _self.navigate(currentPage, {replace: true});
-            } else {
-                _self.navigate('#/', {replace: true});
-            }
-        });
-
-        $('.nav-item').bind('click', function (event) {
-            event.preventDefault();
-            var id = $(this).data('target');
-            _self._scrolling = false;
-            _self.navigate(id, {replace: true});
-        });
-
-    },
-    jumpToPage: function (id) {
-        var _self = this;
-
-        if (id == this.project.currentProjectId) return;
-        _self.hideProject();
-
-        if (_self._scrolling) return;
-
-        var jumpTo = $("#" + id).offset().top;
-
-        TweenLite.to($('html, body'), 1, {
-            scrollTop: jumpTo,
-            ease: Expo.easeInOut
-        });
-
-    },
-    showProject: function (id) {
-        this.jumpToPage('work');
-        if (isMobile.matches) {
-            this.project.$el.removeClass('invisible');
-        }
-        this.project.showProject(id);
-    }
-});
 
 $(document).ready(function () {
 
     var wr = new MainRouter();
     var contact = new Contact();
     var contactForm = new ContactView({model: contact});
-
+    var self = this;
     // VIDEO START
 
     var $siteBg = $("#siteBg");
+
     var $video = $('#myPlayer', $siteBg);
+
+    // INTRO LOADING ANIMATION
     var svgLogo = $('polygon', '.site-logo');
     var titleHeader = $('span', '.brand-heading');
-
-    $('img.lazy').lazyload({
-        effect: "fadeIn"
-    });
-
     var colors = ['#86a4ce', '#5e6870', '#3c444b'];
     var currentColor = 0;
-    var self = this;
     var loadLoop = function () {
         self.loader = setInterval(function () {
             currentColor++;
@@ -117,8 +42,10 @@ $(document).ready(function () {
             }
             svgLogo.css({fill: colors[currentColor]});
             titleHeader.css({color: colors[currentColor]});
-        }, 300);
+        }, 400);
     };
+
+    loadLoop();
 
     var showSite = function () {
         clearInterval(self.loader);
@@ -132,14 +59,15 @@ $(document).ready(function () {
         });
     };
 
-    loadLoop();
-
     $video.on('canplay', showSite);
 
     if ($video[0].readyState > 3) {
         showSite();
     }
 
+    $('img.lazy').lazyload({
+        effect: "fadeIn"
+    });
 
     // VIDEO END
 
