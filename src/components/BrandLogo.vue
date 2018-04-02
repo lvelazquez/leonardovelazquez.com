8cbb78d6a919d6b9a0e5fd64caf660be1956bfaa
lvelazquez <leo@leonardovelazquez.com>
2018-04-01 19:41:42 -0700

    brand logo

---
+++ src/components/BrandLogo.vue
@@ -0,0 +1,61 @@
+<template>
+    <div :class="`brand-logo ${color}-logo`" :style="`font-size:${fontSize}`">
+        <Logo :width="`${logoWidth}`" :height="`${logoHeight}`"/>
+        <h1 class="title">
+            <span class="titleLoader">Leonardo Vel&aacute;zquez</span>
+            <span class="subheader titleLoader">Software Engineer</span></h1>
+    </div>
+</template>
+
+<script>
+import Logo from "./Logo";
+
+export default {
+  name: "BrandLogo",
+  components: {
+    Logo
+  },
+  props: {
+    logoWidth: { type: String, default: "4.5rem" },
+    logoHeight: { type: String, default: "4.25rem" },
+    color: { type: String, default: "white" },
+    fontSize: { type: String }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../settings";
+.white-logo {
+  svg {
+    fill: $white-color;
+  }
+  color: $white-color;
+}
+.black-logo {
+  svg {
+    fill: $black-color;
+  }
+  color: $black-color;
+}
+.brand-logo {
+  display: flex;
+  text-transform: uppercase;
+  line-height: 0.95;
+  margin-bottom: 5px;
+  .logo {
+    margin-right: 5px;
+    margin-top: 4px;
+  }
+  .title {
+    font-family: "Montserrat";
+    font-weight: 700;
+    font-size: 2.6rem;
+    white-space: nowrap;
+    width: 80%;
+    span {
+      display: block;
+    }
+  }
+}
+</style>
--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,5 +1,5 @@
 <template>
-    <svg class="logo"  version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" :width="`${width}rem`" :height="`${height}rem`" :viewBox="`0 0 68.715 71.741`" enable-background="new 0 0 68.715 71.741" xml:space="preserve">
+    <svg class="logo"  version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" :width="`${width}`" :height="`${height}`" viewBox="0 0 68.715 71.741" enable-background="new 0 0 68.715 71.741" xml:space="preserve">
                         <polygon id="svgLogo" class="logoLoader" points="68.354,0.556 32.797,55.055 67.115,55.055 67.115,71.741
 	0,71.741 0.175,0.556 0.175,0 16.862,0 16.862,44.421 45.832,0.556 46.197,0 68.715,0 "/>
                     </svg>
@@ -9,11 +9,8 @@
 export default {
   name: "Logo",
   props: {
-    width: { type: Number, default: "68.715" },
-    height: { type: Number, default: "71.741px" }
+    width: { type: String, default: "68.715px" },
+    height: { type: String, default: "71.741px" }
   }
 };
 </script>
-
-<style scoped>
-</style>
