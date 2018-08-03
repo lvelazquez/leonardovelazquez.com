0231e894f9ff15e08232de201abae49381866303
Leonardo Velázqez <leo@leonardovelazquez.com>
2018-08-02 19:46:30 -0700

    posting email

--- server/index.js
+++ server/index.js
@@ -6,9 +6,11 @@ const bodyParser = require("body-parser");
 const config = require("./config");
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post("/contact", function(req, res) {
+app.post("/contact", function (req, res, next) {
   let mailOpts, smtpTrans;
   smtpTrans = nodemailer.createTransport({
     host: "smtp.gmail.com",
@@ -21,17 +23,18 @@ app.post("/contact", function(req, res) {
   });
   mailOpts = {
     from: req.body.name + " &lt;" + req.body.email + "&gt;",
-    to: config.GMAIL_USER,
+    to: `${config.GMAIL_USER}@gmail.com`,
     subject: `leonardovelazquez.com: ${req.body.title}`,
     text: `${req.body.name} (${req.body.email}) says: ${req.body.message}`
   };
-  smtpTrans.sendMail(mailOpts, function(error, response) {
+  smtpTrans.sendMail(mailOpts, function (error) {
     if (error) {
-      res.render("contact-failure");
+      res.status(500);
+      res.json(JSON.stringify({ok: false, error: error}));
     } else {
-      res.render("contact-success");
+      res.json(JSON.stringify({ok: true, error: false}));
     }
   });
 });
 
-app.listen(3000, () => console.log('Server listening on port 3000!'));
+app.listen(3000, () => console.log("Server listening on port 3000!"));
--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -37,7 +37,10 @@
                 SUBMIT
               </button>
             </form>
-            <div class="warning-text form-text" v-if="!isValid && hasSubmit">All fields are required.</div>
+            <div class="warning-text form-text" v-if="!isValid || !isSuccess && hasSubmit">{{errorMessage}}</div>
+            <p class="email-text" v-if="!isSuccess && hasSubmit">Please try again later or reach me at <a
+              href="mailto:leo@leonardovelazquez.com">leo@leonardovelazquez.com</a>
+            </p>
             <div class="form-text" v-if="isSuccess">Successfully sent!</div>
           </div>
         </div>
@@ -63,177 +66,186 @@
 </template>
 
 <script>
-import Map from "../components/Map";
-import { every } from "lodash";
+  import Map from "../components/Map";
+  import {every} from "lodash";
 
-export default {
-  name: "Contact",
-  props: {
-    isLoaded: Boolean
-  },
-  components: {
-    Map
-  },
-  data() {
-    return {
-      hasSubmit: false,
-      isValid: false,
-      isSuccess: false,
-      contact: {
-        name: "",
-        email: "",
-        title: "",
-        message: ""
-      }
-    };
-  },
-  methods: {
-    async handleSubmit() {
-      this.hasSubmit = true;
-      this.isValid = every(this.contact, value => value !== "");
-      if (this.isValid) {
-        console.log("submit!", this.contact, this.isValid);
-        this.isSuccess = true;
+  export default {
+    name: "Contact",
+    props: {
+      isLoaded: Boolean
+    },
+    components: {
+      Map
+    },
+    data() {
+      return {
+        hasSubmit: false,
+        isValid: false,
+        isSuccess: false,
+        errorMessage: 'All fields are required.',
+        contact: {
+          name: "",
+          email: "",
+          title: "",
+          message: ""
+        }
+      };
+    },
+    methods: {
+      async handleSubmit() {
+        this.hasSubmit = true;
+        this.isValid = every(this.contact, value => value !== "");
+        if (this.isValid) {
 
-        const response = await fetch("/contact", {
-          body: JSON.stringify(this.contact), // must match 'Content-Type' header
-          cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-          credentials: "same-origin", // include, same-origin, *omit
-          headers: {
-            "user-agent": "Mozilla/4.0 MDN Example",
-            "content-type": "application/json"
-          },
-          method: "POST", // *GET, POST, PUT, DELETE, etc.
-          mode: "cors", // no-cors, cors, *same-origin
-          redirect: "follow", // manual, *follow, error
-          referrer: "no-referrer" // *client, no-referrer
-        });
-
-        if (response.ok) {
-          console.log("isSent!", response);
-        } else {
-          console.log("some error", response);
+          const response = await fetch("/contact", {
+            body: JSON.stringify(this.contact), // must match 'Content-Type' header
+            cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+            // credentials: "same-origin", // include, same-origin, *omit
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json'
+            },
+            method: "POST", // *GET, POST, PUT, DELETE, etc.
+            //mode: "cors", // no-cors, cors, *same-origin
+            //redirect: "follow", // manual, *follow, error
+            //referrer: "no-referrer" // *client, no-referrer
+          });
+          const res = JSON.parse(await response.json());
+          if (res.ok) {
+            this.isSuccess = true;
+          } else {
+            console.error(res.error);
+            this.isSuccess = false;
+            this.errorMessage = "There's been a server side error."
+          }
         }
       }
     }
-  }
-};
+  };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-@import "../styles/settings";
-@import "../styles/content";
-@import "../styles/media-queries";
+  @import "../styles/settings";
+  @import "../styles/content";
+  @import "../styles/media-queries";
 
-.banner-social-buttons {
-  display: flex;
-  text-align: center;
-  margin: 0 auto;
-  flex-direction: column;
-  li {
-    flex: 1;
-    display: inline-block;
-    margin-bottom: 1rem;
-  }
-  @media (min-width: $bp-ms) {
-    flex-direction: row;
-    max-width: 30rem;
+  .banner-social-buttons {
+    display: flex;
+    text-align: center;
     margin: 0 auto;
+    flex-direction: column;
+    li {
+      flex: 1;
+      display: inline-block;
+      margin-bottom: 1rem;
+    }
+    @media (min-width: $bp-ms) {
+      flex-direction: row;
+      max-width: 30rem;
+      margin: 0 auto;
+    }
   }
-}
 
-.btn {
-  font-family: "Montserrat", Arial, sans-serif;
-  max-width: 10rem;
-  text-align: center;
-  display: inline-block;
-  text-transform: uppercase;
-  border: solid $light-gray-color 1px;
-  border-radius: 6px;
-  color: $black-color;
-  background-color: transparent;
-  transition: all 0.1s ease-in-out;
-  padding: 10px 16px;
-  font-size: 18px;
-  line-height: 1.33;
-  i {
+  .btn {
+    font-family: "Montserrat", Arial, sans-serif;
+    max-width: 10rem;
+    text-align: center;
+    display: inline-block;
+    text-transform: uppercase;
+    border: solid $light-gray-color 1px;
+    border-radius: 6px;
     color: $black-color;
+    background-color: transparent;
+    transition: all 0.1s ease-in-out;
+    padding: 10px 16px;
+    font-size: 18px;
+    line-height: 1.33;
+    i {
+      color: $black-color;
+    }
   }
-}
 
-.btn:hover {
-  color: $white-color;
-  background-color: $black-color;
-  i {
+  .btn:hover {
     color: $white-color;
+    background-color: $black-color;
+    i {
+      color: $white-color;
+    }
   }
-}
 
-#contact {
-  color: $black-color;
-  background-color: $white-color;
-  padding: 0;
-}
-
-.contact-form {
-  max-width: 15rem;
-  margin: 0 auto;
-  text-align: center;
-  padding: 0 2rem 4rem;
-  @media (min-width: $bp-sm) {
-    padding: 0 3rem 4rem;
+  #contact {
+    color: $black-color;
+    background-color: $white-color;
+    padding: 0;
   }
-  @media (min-width: $bp-md) {
-    padding: 0 10rem 4rem;
+
+  .contact-form {
+    max-width: 15rem;
+    margin: 0 auto;
+    text-align: center;
+    padding: 0 2rem 4rem;
+    @media (min-width: $bp-sm) {
+      padding: 0 3rem 4rem;
+    }
+    @media (min-width: $bp-md) {
+      padding: 0 10rem 4rem;
+    }
   }
-}
 
-.contact-wrapper {
-  padding-bottom: 3rem;
-}
-
-#submit {
-  font-weight: 600;
-  background-color: $blue-color;
-  color: $white-color;
-  cursor: pointer;
-  transition: background-color 0.25s ease-out;
-  &:hover {
-    background-color: darken($blue-color, 25%);
+  .contact-wrapper {
+    padding-bottom: 3rem;
   }
-}
 
-.input-group {
-  width: 100%;
-  border: solid $light-gray-color 2px;
-  border-radius: 5px;
-  margin-bottom: 1rem;
-  outline: none;
-}
+  #submit {
+    font-weight: 600;
+    background-color: $blue-color;
+    color: $white-color;
+    cursor: pointer;
+    transition: background-color 0.25s ease-out;
+    &:hover {
+      background-color: darken($blue-color, 25%);
+    }
+  }
 
-.input-field {
-  font-family: "Open Sans", Helvetica, sans-serif;
-  width: 100%;
-  padding: 0.5rem;
-  outline: none;
-  box-shadow: none;
-  background: none;
-  border: none;
-}
+  .input-group {
+    width: 100%;
+    border: solid $light-gray-color 2px;
+    border-radius: 5px;
+    margin-bottom: 1rem;
+    outline: none;
+  }
 
-.warning {
-  border: solid $warning-color 2px;
-}
-.form-text {
-  color: $blue-color;
-  font-family: "Open Sans", Helvetica, sans-serif;
-  font-weight: 700;
-  font-size: 1.2rem;
-  white-space: nowrap;
-}
+  .input-field {
+    font-family: "Open Sans", Helvetica, sans-serif;
+    width: 100%;
+    padding: 0.5rem;
+    outline: none;
+    box-shadow: none;
+    background: none;
+    border: none;
+  }
 
-.warning-text {
-  color: $warning-color;
-}
+  .warning {
+    border: solid $warning-color 2px;
+  }
+
+  .form-text {
+    color: $blue-color;
+    font-family: "Open Sans", Helvetica, sans-serif;
+    font-weight: 700;
+    font-size: 1.2rem;
+    white-space: nowrap;
+  }
+
+  .warning-text {
+    color: $warning-color;
+  }
+
+  .email-text {
+    font-family: Montserrat;
+    font-weight: 700;
+    font-size: 1.1rem;
+  }
+
 </style>
