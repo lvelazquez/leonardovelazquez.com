aa5ea3530db2704c1aa8ffb74e9d3d72f43d608b
Leonardo Velázqez <leo@leonardovelazquez.com>
2018-08-03 11:14:03 -0700

    setup build for heroku

--- server/index.js
+++ server/index.js
@@ -6,9 +6,17 @@ const bodyParser = require("body-parser");
 const config = require("./config");
 
 const app = express();
+const port = process.env.PORT || 3000;
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static(__dirname + "/dist"));
+
+app.get(/.*/, function (req, res) {
+  res.sendFile(__dirname + 'dist/index.html');
+});
 
 app.post("/contact", function (req, res, next) {
   let mailOpts, smtpTrans;
@@ -37,4 +45,4 @@ app.post("/contact", function (req, res, next) {
   });
 });
 
-app.listen(3000, () => console.log("Server listening on port 3000!"));
+app.listen(port, () => console.log(`Server listening on port ${port}!`));
