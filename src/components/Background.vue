37f39658579013a591c46508062c5a538a74a962
Leonardo Velázqez <leo@leonardovelazquez.com>
2018-08-03 17:24:32 -0700

    fixed video issue

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -1,5 +1,5 @@
 <template>
-    <video id="site-background" v-once :poster="`${cloudinaryUrl}video/upload/lakehi.png`" autoplay preload loop>
+    <video id="site-background" v-once :poster="`${cloudinaryUrl}video/upload/lakehi.png`" autoplay preload loop muted>
         <source v-once :src="`${cloudinaryUrl}video/upload/lakehi.mp4`" type="video/mp4"/>
     </video>
 </template>
