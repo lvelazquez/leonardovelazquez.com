3a5e9dc1f070cb961258389387f9b125568bdf80
Leonardo Velázquez <leo@leonardovelazquez.com>
2019-02-21 16:26:47 -0800

    email validation test works

--- tests/unit/contact.spec.js
+++ tests/unit/contact.spec.js
@@ -9,5 +9,33 @@ describe('Contact.vue', () => {
     expect(wrapper.contains('section#contact')).toBe(true)
   });
 
+  it('email validation works', () => {
+    const wrapper = mount(Contact, {
+      attachToDocument: true
+    });
+    const emails = {
+      incorrect: ['ar', '', ' ', 'lalo@lol', ' leo@amazero.com'],
+      correct:['leo@amazero.com', 'lula@lolli.co.so']
+    };
+
+    const incorrectEmails = emails.incorrect.every(wrapper.vm.validateEmail);
+    expect(incorrectEmails).toBe(false);
+
+    const correctEmails = emails.correct.every(wrapper.vm.validateEmail);
+    expect(correctEmails).toBe(true);
+
+  });
+
+  it('validates name', () => {
+
+  });
+
+  it('throws error if email is invalid', () => {
+
+  });
+
+  it('submits form', () => {
+
+  });
 
 });
